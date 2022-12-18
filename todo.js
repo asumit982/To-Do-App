6094c3b29bb6fc4d0197f27fbfc7bf3dbeb89a49
sumittricon <sumit.anshul@triconinfotech.com>
2022-12-18 11:16:12 +0530

    Added completeTask function

--- todo.js
+++ todo.js
@@ -7,7 +7,21 @@ console.log('Working');
 
 function renderList () {}
 
-function markTaskAsComplete (taskId) {}
+function markTaskAsComplete (taskId) {
+    const task = tasks.filter(function(task){
+        return task.id === taskId
+    });
+
+    if(task.length > 0){
+        const currentTask = task[0];
+        currentTask.done = !currentTask.done;
+        renderList();
+        showNotification('Task completed successfully');
+        return;
+    }
+
+    showNotification('Could not complete the task');
+}
 
 function deleteTask (taskId) {
     const newTasks = tasks.filter(function(task){
